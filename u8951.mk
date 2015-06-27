79abacfd37aa20fc8bf0516ede523767fcdbccbf
William <wbrambley@gmail.com>
2015-06-27 21:14:41 +1200

    First commit

---
+++ u8951.mk
@@ -0,0 +1,71 @@
+#
+# Copyright 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PACKAGE_OVERLAYS += device/huawei/u8951/overlay
+
+# NFC packages
+PRODUCT_PACKAGES += \
+    NfcNci \
+    nfc_nci.pn54x.default \
+    Tag \
+    libnfc \
+    libnfc-nxp \
+    libnfc-nci \
+    libnfc_jni \
+    com.android.nfc_extras \
+    libnfc_ndef \
+    SmartcardService \
+    org.simalliance.openmobileapi \
+    com.android.qcom.nfc_extras \
+    NfcTests
+    
+# NFCEE access control
+ifeq ($(TARGET_BUILD_VARIANT),user)
+    NFCEE_ACCESS_PATH := device/huawei/u8951/configs/nfcee_access.xml
+else
+    NFCEE_ACCESS_PATH := device/huawei/u8951/configs/nfcee_access_debug.xml
+endif
+PRODUCT_COPY_FILES += \
+    $(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml \
+    $(NFCEE_ACCESS_PATH):system/etc/nfcee_access_debug.xml
+  
+# USB
+PRODUCT_PACKAGES += \
+    com.android.future.usb.accessory
+    
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.dualmic=true \
+    ro.qc.sdk.audio.fluencetype=fluence
+    
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml
+
+PRODUCT_COPY_FILES += \
+    frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml 
+
+
+# Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.confg.hw_appfsversion=U8951V4_4_SYSIMG \
+    ro.confg.hw_appsbootversion=U8951V4_4_APPSBOOT \
+    ro.confg.hw_appversion=U8951V4_4_KERNEL
+
+$(call inherit-product-if-exists, vendor/huawei/u8951/u8951-vendor.mk)
